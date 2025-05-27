edf4e19ca21a57de96721c210a59c5a0abfa738c
Vít Provazník <vit.provaznik.023@pslib.cz>
2025-05-27 10:25:19 +0200

    init

---
+++ src/scripts/main.js
@@ -0,0 +1,3 @@
+import '../styles/main.css'
+
+console.log("Ahoj světe :D")
---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import vituum from 'vituum'
+
+export default {
+    plugins: [
+      vituum({
+        pages: {
+          normalizeBasePath: true
+        }, 
+        imports: {
+            paths: ['./src/styles//**', './src/scripts//**']
+        }
+      })
+    ],
+  }
