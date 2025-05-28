7f4d1cdedca5cfa035c56a371517d8c86708eb6c
Vít Provazník <vit.provaznik.023@pslib.cz>
2025-05-28 11:26:21 +0200

    hero

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,43 @@
 // import '../styles/main.css'
 
-console.log("Ahoj světe :D")
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButton = document.querySelector(`.hamburger-btn`);
+  const hamburgerNav = document.querySelector(".hamburger-nav");
+  const overlay = document.querySelector(".overlay");
+  const body = document.querySelector("html");
+  const menuItems = document.querySelectorAll(".hamburger-nav li"); 
+  const menuIcons = document.querySelectorAll(".hamburger-nav__icon-container a")
+
+  // Toggle active classes when the hamburger button is clicked
+  hamburgerButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    hamburgerButton.classList.toggle("active");
+    hamburgerNav.classList.toggle("active");
+    overlay.classList.toggle("active");
+    body.classList.toggle("no-scroll");
+  });
+
+  // Remove active classes when the overlay is clicked
+  overlay.addEventListener("click", () => {
+    hamburgerButton.classList.remove("active");
+    hamburgerNav.classList.remove("active");
+    overlay.classList.remove("active");
+    body.classList.remove("no-scroll");
+  });
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      hamburgerButton.classList.remove("active");
+      hamburgerNav.classList.remove("active");
+      overlay.classList.remove("active");
+      body.classList.remove("no-scroll");
+    });
+  });
+  menuIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    hamburgerButton.classList.remove("active");
+    hamburgerNav.classList.remove("active");
+    overlay.classList.remove("active");
+    body.classList.remove("no-scroll");
+  });
+});
+});
