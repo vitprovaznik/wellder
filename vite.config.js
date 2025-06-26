c75f2a6cb1344ce933746eac4cd4f89c434a051d
Vít Provazník <vit.provaznik.023@pslib.cz>
2025-06-26 13:24:39 +0200

    vituum

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import vituum from "vituum";
 
 export default {
+  base: "/wellder/",
   plugins: [
     vituum({
       pages: {
         normalizeBasePath: true,
+        base: "/wellder/",
       },
       imports: {
         paths: ["./src/styles//**", "./src/scripts//**"],
